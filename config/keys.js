54ef810f192d91a6f6dfbc6c81b422e2b283b7e4
Tony Carbetta <ruquss@gmail.com>
2018-08-15 19:01:05 -0700

    key change

--- config/keys.js
+++ config/keys.js
@@ -3,5 +3,5 @@ module.exports = {
     "mongodb://<username>:<password>@ds035846.mlab.com:35846/devconnector"
 };
 
-//see authorized user for credentials
+//see authorized user for credentials!
 //<username>:<password>
